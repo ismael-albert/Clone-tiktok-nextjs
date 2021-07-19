4407edc16cd09da845fb080f75f34a6839025943
ismael-albert <albertismael7@gmail.com>
2021-07-19 14:46:44 -0300

    init

---
+++ tiktok-project/src/components/Sidebar/index.js
@@ -0,0 +1,24 @@
+import { Container } from "../Header/styles";
+
+
+function Sidebar(){
+    return;
+    <Container>
+        <MennuItem></MennuItem>
+        <MennuItem></MennuItem>
+
+        <Following>
+            <FollowingHeader>Suas principais contas</FollowingHeader>
+            <User></User>
+        </Following>
+        <InfoContainer>
+        <Links></Links>
+        <Links></Links>
+        <Links></Links>
+        <Links></Links>
+        <Links></Links>
+        </InfoContainer>
+
+    </Container>
+}
+export default Sidebar;
---
+++ tiktok-project/src/pages/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return <Layout></Layout>
+}
