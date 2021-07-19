80b9f18a666223d312e116dabd2cbba41cc1e62b
ismael-albert <albertismael7@gmail.com>
2021-07-19 15:20:26 -0300

    fim-project

--- tiktok-project/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-import Layout from '../components/Layout'
-
-export default function Home() {
-  return <Layout></Layout>
-}
--- tiktok-project/src/styles/theme.js
+++
@@ -1,6 +0,0 @@
- const theme = {
-    colors: {
-      primary: '#fe2c55',
-    },
-  }
-  export default theme;
